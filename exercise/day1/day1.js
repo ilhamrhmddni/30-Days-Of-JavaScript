fc57d2b9de63a18bef45c07b04c9f706393a6a3e
ilharmhddni <ilhamrhmddni0@gmail.com>
2023-07-03 12:48:08 +0800

    update day1

---
+++ exercise/day1/day1.js
@@ -0,0 +1,17 @@
+
+// Comment can make code readble
+
+// Welcome to 30DaysOfJavascript
+
+/*
+Comment can make code readable
+easy to reuse and informative
+*/
+
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+// Declare four variables without assigning values
+// Declare four variables with assigned values
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
