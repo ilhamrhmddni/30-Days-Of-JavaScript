255215e537b9f58f0367e79cce9b62aabb3ce028
ilharmhddni <ilhamrhmddni0@gmail.com>
2023-07-03 20:25:43 +0800

    level2

---
+++ exercise/day2/level2.js
@@ -0,0 +1,12 @@
+// Number 1
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.")
+
+// Number 2 
+console.log(`\"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.\"`)
+
+// Number 3
+const x = 10
+const y = '10'
+console.log(typeof(x))
+console.log(typeof(y))
+console.log(x == y)
